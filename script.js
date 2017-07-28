5a91a66e6ca4f822050f57322197c5c5290ba4f8
Michael Edwards <edwardsmichaelv@gmail.com>
2017-07-27 21:15:47 -0400

    Fixed variable names.

--- script.js
+++ script.js
@@ -2,10 +2,8 @@ var http = require('http');
 var obj = require('./textObject');
 
 http.createServer(function(request, response) {
-  var asciiArt = obj.randomizer();
-  console.log(asciiArt);
+  var quote = obj.randomizer();
   response.writeHead(200, { "Content-type": "text/plain" });
-  response.write(asciiArt);
-  // console.log(asciiArt.randomProp);
+  response.write(quote);
   response.end();
 }).listen(8888);
--- textObject.js
+++ textObject.js
@@ -1,9 +1,9 @@
 var obj = {
-  alien: "/“Don/'t cry because it/'s over, smile because it happened./” ― Dr. Seuss",
-  wolf: '"Be yourself; everyone else is already taken." ― Oscar Wilde',
-  flamingo: '“You only live once, but if you do it right, once is enough.” ― Mae West',
+  seuss: "/“Don/'t cry because it/'s over, smile because it happened./” ― Dr. Seuss",
+  wilde: '"Be yourself; everyone else is already taken." ― Oscar Wilde',
+  west: '“You only live once, but if you do it right, once is enough.” ― Mae West',
   randomizer: function(){
-    var propArr = [obj.alien, obj.wolf, obj.flamingo];
+    var propArr = [obj.seuss, obj.wilde, obj.west];
     return propArr[Math.floor(Math.random() * propArr.length)];
   }
 };
